bad6ed8c4348c9e7627e7117286bc926a655b6c9
Udit Gami <123604132+Udit3944@users.noreply.github.com>
2024-09-06 22:55:12 -0500

    Update script.js

--- Portfolio-Udit-main (1)/Portfolio-Udit-main/UditGamiPortfolio/Portfolio-Website-main/assets/js/script.js
+++ Portfolio-Udit-main (1)/Portfolio-Udit-main/UditGamiPortfolio/Portfolio-Website-main/assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | UditGami";
+            document.title = "Portfolio | UditGami Portfolio";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
